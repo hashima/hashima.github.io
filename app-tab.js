1a763470d64249d1a50981bf0406805dd843a1ed
hashima <hashima@users.noreply.github.com>
2019-09-19 17:32:03 +0900

    0

--- app-tab.js
+++ app-tab.js
@@ -2,7 +2,7 @@ const homePage = {
   template: '#home',
   props: ['score','itemsbatter','selectedItemBatter'],
   methods: {
-    fetchBatter() {
+    fetchBatter(event) {
       vm.fetchBatter();
     },
     // onChangeEvent(e){
@@ -69,9 +69,9 @@ var vm = new Vue({
     md() {
       return this.$ons.platform.isAndroid();
     },
-    fetchBatter: function() {
-      alert(this.tabs[0].props.selectedItemBatter);
-    },
+    // fetchBatter: function() {
+    //   alert(this.tabs[0].props.selectedItemBatter);
+    // },
   },
   computed: {
     title() {
