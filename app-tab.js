ba8711eaf9586c1df5dd9eec29bd3da3238b0f62
hashima <hashima@users.noreply.github.com>
2019-09-20 12:58:09 +0900

    Update app-tab.js

--- app-tab.js
+++ app-tab.js
@@ -55,7 +55,7 @@ var vm = new Vue({
   },
   mounted: function () {
     axios.get("./score.json").then(response => (this.tabs[0].props.score = response.data));
-    // this.tabs[0].props.selectedItem = 'item2';
+    this.tabs[0].props.selectedItem = 'item2';
   },
   methods: {
     md() {
