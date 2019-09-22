9f144d2845f010b524104c17e9a323ed27246499
hashima <hashima.brain@gmail.com>
2019-09-22 16:42:35 +0900

    Update app-text.js

--- dev/app-text.js
+++ dev/app-text.js
@@ -13,33 +13,12 @@ var vm = new Vue({
   },
   mounted: function () {
     axios.get("./text.json").then(response => (this.textData = response.data));
-    console.log(this.tabs[1].props.score);
+    // console.log(this.tabs[1].props.score);
   },
   methods: {
     md() {
       return this.$ons.platform.isAndroid();
     },
-    fetch: function(e) {
-     if(e.target.value == "item3")
-      {
-        this.tabs[0].props.itemsDirection = [
-          { text: '----', value: '----' },
-          { text: 'item7', value: 'item7' },
-          { text: 'item8', value: 'item8' },
-          { text: 'item9', value: 'item9' },
-        ];
-      }
-      // this.shown = false;
-      // this.tabs[0].props.show = false;
-    },
-    showSelect: function(e) {
-      return this.shown;
-    },
-    fetch2: function(e) {
-    },
-    setOfficial: function(e) {
-      this.official = e.target.value;
-    }
   },
   computed: {
     // title() {
