de1a70ac351abc0dffdf6b995efa33159e49c8ca
hashima <hashima@1979-nice.com>
2019-09-29 06:37:40 +0900

    0

--- dev/app-text.js
+++ dev/app-text.js
@@ -4,9 +4,9 @@ var vm = new Vue({
   template: '#main',
   data() {
     return {
-      activeIndex: 1,
       title: "ScoreBook.mobi 開発版",
-      textData: [],
+      textData: null,
+      state: 'initial',
     };
   },
   mounted: function () {
@@ -26,8 +26,47 @@ var vm = new Vue({
     },
   },
   computed: {
-    // title() {
-    //   return this.tabs[this.activeIndex].label;
-    // },
+    inning: function(){
+      if(this.textData != null){
+        return this.textData.scoreboard.inning;
+      }else{
+        return [null,null,null,null,null,null,null,];
+      }
+    },
+    topname: function(){
+      if(this.textData != null){
+        return this.textData.scoreboard.topname;
+      }else{
+        return "&nbsp;";
+      }
+    },
+    top: function(){
+      if(this.textData != null){
+        return this.textData.scoreboard.top;
+      }else{
+        return [null,null,null,null,null,null,null,];
+      }
+    },
+    bottomname: function(){
+      if(this.textData != null){
+        return this.textData.scoreboard.bottomname;
+      }else{
+        return "&nbsp;";
+      }
+    },
+    bottom: function(){
+      if(this.textData != null){
+        return this.textData.scoreboard.bottom;
+      }else{
+        return [null,null,null,null,null,null,null,];
+      }
+    },
+    texts: function(){
+      if(this.textData != null && this.textData.texts != null){
+        return this.textData.texts;
+      }else{
+        return [];
+      }
+    }
   }
 });
