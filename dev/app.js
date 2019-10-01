c305e0a91cb2d0f1b943174ad894d7cd05129af0
hashima <hashima@users.noreply.github.com>
2019-10-01 17:34:01 +0900

    Update app.js

--- dev/app.js
+++ dev/app.js
@@ -355,13 +355,13 @@ const offensePage = Vue.component('offense-page', {
         this.showSelected1stRunnerBase = false
         this.$emit('showSelected1stRunnerBase')
         this.$emit('selected1stRunner')
-        console.log(this.selected1stRunner)
         if(this.selected1stRunner == "盗塁")
         {
           this.selected1stBase = "2塁へ"
           if(this.runner2nd != null){
             this.selected2ndRunner = "盗塁"
             this.$emit('selected2ndRunner')
+            console.log(this.selected2ndRunner)
             this.selected2ndBase = "3塁へ"
             this.$emit('selected2ndBase')
             if(this.runner3rd != null){
