aac3edb877bd0c886b94eb79b1e154465a8ee044
hashima <hashima@users.noreply.github.com>
2019-10-10 18:53:25 +0900

    Update app.js

--- dev/app.js
+++ dev/app.js
@@ -656,7 +656,8 @@ const topPage = Vue.component('top-page', {
         let p = this.member[i];
         if (p.order > 0) {
           // mems.push(p);
-          mems[p.order] = p
+          mems[p.order] = p;
+          mems.push(p);
         }
       }
       return mems;
