124383525d640a30b3013552753e87c50438666f
hashima <hashima@users.noreply.github.com>
2019-10-10 17:09:50 +0900

    Update app.js

--- dev/app.js
+++ dev/app.js
@@ -617,17 +617,16 @@ const topPage = Vue.component('top-page', {
         }
   },
   created: function() {
-    axios.get("./membertop.json").then(response => (this.member = response.data));
-    console.log('created');
-    console.log(this.member);
+    axios.get("./membertop.json")
+         .then(response => (this.member = response.data))
+         .then(console.log('created'))
+         .then(console.log(this.member));
   },
   mounted: function () {
     axios.get("./toporder.json")
-         .then(response => (this.toporder = response.data.order, this.reserve = response.data.reserve)
-         .then(
-    console.log('mounted'),
-    console.log(this.toporder)
-    ));
+         .then(response => (this.toporder = response.data.order, this.reserve = response.data.reserve))
+         .then(console.log('mounted'))
+         .then(console.log(this.toporder));
   },
   methods: {
     buttonPChange: function() {
